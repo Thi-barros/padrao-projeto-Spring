9e152c710a1018971aa035775e00bacadb92bd68
Thiago Barros <thiagoo.barros08@gmail.com>
2024-11-17 01:38:05 -0300

    first commit

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/model/EnderecoRepository.java
@@ -0,0 +1,6 @@
+package one.digitalinnovation.lab_padroes_projeto_spring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
